b8169bbbca89e372c8fa6389513bb134c338fd46
Gaurav <gaurav.ssnl@gmail.com>
2020-03-14 15:35:55 +0530

    Added shorter version of fibonacci generator

--- python/Data Structures and Algorithms in Python Book/primer/fibonacci_shorter.py
+++ python/Data Structures and Algorithms in Python Book/primer/fibonacci_shorter.py
@@ -1,11 +1,12 @@
 def fibonacci():
     a, b = 0, 1
-    yield a 
-    a, b = b, a+b
+    while True:
+        yield a 
+        a, b = b, a+b
 
 
 if __name__ == '__main__':
     n = 20
     fibonacci_gen = fibonacci()
-    for num  in fibonacci_gen:
-        print(num)
+    for i   in range(20):
+        print(next(fibonacci_gen))
