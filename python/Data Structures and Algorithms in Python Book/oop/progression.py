52f033318778e07cfb0cb4a793f290c0d4f9c03e
Gaurav <gaurav.ssnl@gmail.com>
2020-03-15 13:43:05 +0530

    made data members protected instead of private

--- python/Data Structures and Algorithms in Python Book/oop/progression.py
+++ python/Data Structures and Algorithms in Python Book/oop/progression.py
@@ -2,16 +2,16 @@ class Progression:
     """Iterator producing a generic progression"""
 
     def __init__(self, start=0):
-        self.__current = start
+        self._current = start
 
     def _advance(self):
-        self.__current += 1
+        self._current += 1
     
     def __next__(self):
-        if self.__current is None:
+        if self._current is None:
             raise StopIteration()
         else:
-            answer = self.__current
+            answer = self._current
             self._advance()
             return answer
     
