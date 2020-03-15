6c3850e83b9783d4ee99fb458785f782057203f2
Gaurav <gaurav.ssnl@gmail.com>
2020-03-16 00:37:34 +0530

    use sorting to find uniqueness

---
+++ python/Data Structures and Algorithms in Python Book/analysis/unique2.py
@@ -0,0 +1,9 @@
+# Using Sorting
+# Overall complexity: O(n log n ) as sorted() has that complexity
+
+def unique2(S):
+    temp = sorted(s) 
+    for j in range(1, len(S)): # O(n)
+        if S[j-1] == S[j]: # O(1)
+            return False
+    return True 
