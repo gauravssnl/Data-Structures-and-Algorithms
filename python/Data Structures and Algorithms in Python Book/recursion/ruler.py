eaf77ed6a0bb2e69db23684837c6add28128c43e
Gaurav <gaurav.ssnl@gmail.com>
2020-03-16 18:41:01 +0530

    Added recursive example to draw ruler

--- python/Data Structures and Algorithms in Python Book/recursion/ruler.py
+++ python/Data Structures and Algorithms in Python Book/recursion/ruler.py
@@ -1,3 +1,4 @@
+# For c ≥ 0, a call to draw interval(c) results in precisely 2c−1 lines of output.
 def draw_line(tick_length, tick_label=''):
     line = '-' * tick_length
     if tick_label:
@@ -10,12 +11,14 @@ def draw_interval(center_tick_length):
         draw_line(center_tick_length)
         draw_interval(center_tick_length-1)
 
-def draw_ruler(num_inches, major_tick_length):
+def draw_ruler(num_inches, major_tick_length):  
     draw_line(major_tick_length, '0') # draw '0' line
 
     for j in range(1, num_inches + 1):
         draw_interval(major_tick_length - 1)
         draw_line(major_tick_length, str(j))
+
+# For c ≥ 0, a call to draw interval(c) results in precisely 2 ** c −1 lines of output.    
 
 if __name__ == "__main__":
     num_inches = 2
