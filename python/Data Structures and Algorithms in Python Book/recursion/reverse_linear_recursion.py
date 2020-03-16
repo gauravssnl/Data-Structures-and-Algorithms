999ecaa160424e15311741482ea92970b099548e
Gaurav <gaurav.ssnl@gmail.com>
2020-03-16 19:40:46 +0530

    Added linear recursive example to reverse sequence

---
+++ python/Data Structures and Algorithms in Python Book/recursion/reverse_linear_recursion.py
@@ -0,0 +1,11 @@
+def reverse(S, start, stop):
+    """Reverse elements in S[start:stop] recursively"""
+    if start < stop - 1:    # at least 2 elements
+        S[start], S[stop-1] = S[stop-1], S[start]   # swap first with last and last with first
+        return reverse(S, start+1, stop -1)         # recursive call on others
+
+if __name__ == "__main__":
+    s = list(range(1, 21))
+    print("Original Sequence:", s)
+    reverse(s, 0, len(s))
+    print("New Sequence:", s)
