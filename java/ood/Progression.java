dae3068882f5a57d64ddcc9996bfd5f62d68a0fb
Gaurav <gaurav.ssnl@gmail.com>
2020-09-29 00:05:51 +0530

    General numeric progression Class example added

---
+++ java/ood/Progression.java
@@ -0,0 +1,41 @@
+package ood;
+
+/**
+ * Generates a simple progression. By default: 0, 1, 2, ...
+ */
+public class Progression {
+
+    // instance variable
+    protected long current;
+
+    /** Construct a Progression starting at zero. */
+    public Progression() {
+        this(0);
+    }
+
+    /** Construct a progression with given start value. */
+    public Progression(long start) {
+        current = start;
+    }
+
+    /** Return the next value of the progression. */
+    public long nextValue() {
+        long answer = current;
+        advance(); // this protected call is responsible for advancing the current value
+        return answer;
+    }
+
+    /** Advances the current value to the next value of the progression. */
+    protected void advance() {
+        current++;
+    }
+
+    /** Prints the next n values of the Progression separated by spaces. */
+    public void printProgression(int n) {
+        System.out.print(nextValue()); // print first value without space
+        for (int j = 1; j < n; j++) {
+            System.out.println(" " + nextValue());
+        }
+        System.out.println(); // end the line
+    }
+}
