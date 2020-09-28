aefca3db223e18b7bd699700f6a58fa13e539674
Gaurav <gaurav.ssnl@gmail.com>
2020-09-28 13:58:15 +0530

    To  show that parent ref obj is not child instance

--- java/primer/DynamicDispatchDemo.java
+++ java/primer/DynamicDispatchDemo.java
@@ -5,6 +5,7 @@ public class DynamicDispatchDemo {
         CreditCard card;
         card = new CreditCard("Gaurav", "CITI", "123", 20000, 100);
         System.out.println(card);
+        System.out.println(card instanceof PredatoryCreditCard);
         System.out.println(card instanceof CreditCard);
 
         card = new PredatoryCreditCard("Gaurav", "CITI", "123", 30000, 200, 0.685);
