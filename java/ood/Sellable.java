6e9c28832d496c8e140023af9de685f6aeac3679
Gaurav <gaurav.ssnl@gmail.com>
2020-09-29 23:06:33 +0530

    Sellable interface example added

---
+++ java/ood/Sellable.java
@@ -0,0 +1,13 @@
+package ood;
+
+/** Interface for objects that can be sold. */
+public interface Sellable {
+    /** Returns a description of the object. */
+    public String description();
+
+    /** Returns the list price in cents. */
+    public int listPrice();
+
+    /** Returns the lowest price in cents we will aceept. */
+    public int lowestPrice();
+}
