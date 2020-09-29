a28694560f8b677990ffc14b4bb5d1f24145d568
Gaurav <gaurav.ssnl@gmail.com>
2020-09-29 23:52:36 +0530

    Abstract class example added

---
+++ java/ood/AbstractProgression.java
@@ -0,0 +1,29 @@
+package ood;
+
+public abstract class AbstractProgression {
+    protected long current;
+
+    public AbstractProgression() {
+        this(0);
+    }
+
+    public AbstractProgression(long start) {
+        current = start;
+    }
+
+    public long nextValue() {
+        long answer = current;
+        advance(); // an example of the template method pattern
+        return answer;
+    }
+
+    public void printProgression(int n) {
+        System.out.print(nextValue());
+        for (int j = 1; j < n; j++)
+            System.out.print(nextValue());
+        System.out.println();
+    }
+
+    protected abstract void advance(); // abstract method
+
+}
