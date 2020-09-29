5ef0db6889ffb0ec4aea037aa6e0cd21890d3ece
Gaurav <gaurav.ssnl@gmail.com>
2020-09-29 17:49:13 +0530

    ArtithmeticProgression example added

---
+++ java/ood/ArithmeticProgression.java
@@ -0,0 +1,27 @@
+package ood;
+
+public class ArithmeticProgression extends Progression {
+    // store step size
+    protected long increment;
+
+    /** Construct progression 0, 1, 2, ... */
+    public ArithmeticProgression() {
+        this(0, 1);                                                     // start at 0 with increment of 1
+    } 
+
+    /** Construct progression 0, stepsize, 2 * stepsize, ... */
+    public ArithmeticProgression(long stepsize) {
+        this(0, stepsize);                                              // start at 0
+    } 
+
+    /** Construct progression with aribtrary start and increment */
+    public ArithmeticProgression(long start, long stepsize) {
+        super(start);
+        increment = stepsize;
+    }
+
+    @Override
+    protected void advance() {
+        current += increment;
+    }
+}
