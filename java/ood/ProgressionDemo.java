e9d42289667d67cd9c5802a4fa24a80de23360ba
Gaurav <gaurav.ssnl@gmail.com>
2020-09-29 17:50:18 +0530

    Progression & ArithmeticProgression examples added

---
+++ java/ood/ProgressionDemo.java
@@ -0,0 +1,32 @@
+package ood;
+
+public class ProgressionDemo {
+    public static void main(String[] args) {
+        Progression pr = new Progression();
+        System.out.println(pr.current);
+        System.out.println(pr.nextValue());
+        System.out.println(pr.current);
+        pr.printProgression(10);
+
+        pr = new ArithmeticProgression();
+        System.out.println(pr.current);
+        System.out.println(pr.nextValue());
+        System.out.println(pr.current);
+        pr.printProgression(10);
+
+        pr = new ArithmeticProgression(10);
+        System.out.println(pr.current);
+        System.out.println(pr.nextValue());
+        System.out.println(pr.current);
+        pr.printProgression(10);
+
+        pr = new ArithmeticProgression(30);
+        pr.printProgression(10);
+
+        pr = new ArithmeticProgression(1, 2);
+        pr.printProgression(10);
+
+        pr = new ArithmeticProgression(0, 2);
+        pr.printProgression(10);
+    }
+}
