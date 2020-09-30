f6bcdb58e33b6cc10c4e594199144609a0cdfb97
Gaurav <gaurav.ssnl@gmail.com>
2020-10-01 00:35:44 +0530

    SinglyLinkedList example added

---
+++ java/fds/SinglyLinkedList.java
@@ -0,0 +1,98 @@
+package fds;
+
+/** A generic Singly linked list. */
+public class SinglyLinkedList<E> {
+    // nested Node class
+    private static class Node<E> {
+        private E element; // reference to the element stored at this node
+        private Node<E> next; // reference to the susbsequent node in the list
+
+        public Node(E e, Node<E> n) {
+            element = e;
+            next = n;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> n) {
+            next = n;
+        }
+
+        @Override
+        public String toString() {
+            return "Node(" + element + ") -> " + next;
+        }
+
+    }
+
+    // instance variables of SinglyLinkedList class
+    private Node<E> head = null; // head of the list (or null if empty)
+    private Node<E> tail = null; // tail of the list (or null if empty)
+    private int size = 0; // number of nodes in the list
+
+    public SinglyLinkedList() { // construct an empty list initially
+
+    }
+
+    // access methods
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E first() { // returns (but does not remove) the first elelemnt
+        if (isEmpty())
+            return null;
+        return head.getElement();
+    }
+
+    public E last() { // returns (but does not remove) the last element
+        if (isEmpty())
+            return null;
+        return tail.getElement();
+    }
+
+    // update methods
+    public void addFirst(E e) { // adds element e to the first of the list
+        head = new Node<>(e, head);
+        if (size == 0)
+            tail = head; // special case: new node bcomes tail also
+        size++;
+    }
+
+    public void addLast(E e) { // adds element e to the last of the list
+        Node<E> newest = new Node<>(e, null); // new nod will eventually be the tail
+        if (isEmpty())
+            head = newest; // special case: previously empty list
+        else
+            tail.setNext(newest); // new node after existing tail
+        tail = newest; // new node becomes the tail
+        size++;
+    }
+
+    public E removeFirst() { // removes and returns the first element
+        if (isEmpty()) // nothing to remove
+            return null;
+        E answer = head.getElement();
+        head = head.getNext(); // set the head node to the next node of current head
+        size--; // decrease the size
+        if (size == 0)
+            tail = null; // special case: as list is now empty
+        return answer;
+    }
+
+    @Override
+    public String toString() {
+        return "SinglyLinkedList [head=" + head + ", tail=" + tail + ", size=" + size + "]";
+    }
+
+}
