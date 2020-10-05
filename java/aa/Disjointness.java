0daa72e745093fa1df62e17d91da672e5aac2260
Gaurav <gaurav.ssnl@gmail.com>
2020-10-05 23:22:32 +0530

    Add disjoint2 for testing 3-way set disjointness.

--- java/aa/Disjointness.java
+++ java/aa/Disjointness.java
@@ -10,4 +10,15 @@ public class Disjointness {
                         return false;
         return true;
     }
+
+    // Time Complexity : O(n ^ 2)
+    public static boolean disjoint2(int[] groupA, int[] groupB, int[] groupC) {
+        for (int a : groupA)
+            for (int b : groupB)
+                if (a == b)
+                    for (int c : groupC)    // only check C when we find match from A and B
+                        if (a == b && b == c)
+                            return false;
+        return true;
+    }
 }
