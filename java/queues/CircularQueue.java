ff8aabac014ef0356f3be4bb6cbc26d198753bc2
Gaurav <gaurav.ssnl@gmail.com>
2020-10-09 19:12:26 +0530

    CircularQueue with a new rotate( ) method added.

---
+++ java/queues/CircularQueue.java
@@ -0,0 +1,9 @@
+package queues;
+
+public interface CircularQueue<E> extends Queue<E> {
+    /**
+     * Rotates the front element of the queue to the back of the queue This does
+     * nothing if the queue is empty.
+     */
+    void rotate();
+}
