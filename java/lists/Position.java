9b641b3e2b7fc69dd826b824e9785f34bcf89a6e
Gaurav <gaurav.ssnl@gmail.com>
2020-10-12 00:28:54 +0530

    Position interface for Positional List added.

---
+++ java/lists/Position.java
@@ -0,0 +1,11 @@
+package lists;
+
+public interface Position<E> {
+    /**
+     * Returns the element stored at this position.
+     * 
+     * @return the stored element
+     * @throws IllegalStateException if position no longer valid
+     */
+    E getElement() throws IllegalStateException;
+}
