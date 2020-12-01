280900bb21e54faebeba35c9cd8bee0e300203e5
Gaurav <gaurav.ssnl@gmail.com>
2020-12-01 21:24:45 +0530

    Fix array  logic used to find elements are unique.

--- python/Data Structures and Algorithms in Python Book/analysis/unique2.py
+++ python/Data Structures and Algorithms in Python Book/analysis/unique2.py
@@ -4,6 +4,12 @@
 def unique2(S):
     temp = sorted(S) 
     for j in range(1, len(S)): # O(n)
-        if S[j-1] == S[j]: # O(1)
+        if temp[j-1] == temp[j]: # O(1)
             return False
     return True 
+
+if __name__ == "__main__":
+    a1 = [1, 2, 3, 5, 7, 9, 4]
+    print("The elements of {} are {}".format(a1, 'unique' if unique2(a1) else 'not unique'))
+    a2 = [1, 2, 3, 5, 7, 9, 1]
+    print("The elements of {} are {}".format(a2, 'unique' if unique2(a2) else 'not unique'))
