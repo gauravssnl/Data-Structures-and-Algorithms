789d3db3a782b30cf3593cfb66fbdc2a2d163f96
Gaurav <gaurav.ssnl@gmail.com>
2020-12-21 01:26:17 +0530

    A Stack adding /removing elements at 0th index.

---
+++ python/A First Course on Data Structures in Python/ds/stack/badstack.py
@@ -0,0 +1,14 @@
+from stack import ListStack
+
+
+class BadStack(ListStack):
+    def push(self, item):
+        """ Push item to the begining of the list in stack. """
+        self._L.insert(0, item)
+
+    def pop(self):
+        """ Pop item from thr begining of the list in stack. """
+        return self.pop(0)
+
+if __name__ == "__main__":
+    bad_stack = BadStack()
