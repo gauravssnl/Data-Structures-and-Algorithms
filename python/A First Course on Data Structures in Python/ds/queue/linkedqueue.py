9f8e4ec9a9e2309b2e73b2ff6d7003e9108e1b19
Gaurav <gaurav.ssnl@gmail.com>
2020-12-22 02:15:52 +0530

    A Queue implemnted using a LinkedList example.

---
+++ python/A First Course on Data Structures in Python/ds/queue/linkedqueue.py
@@ -0,0 +1,22 @@
+from ..linkedlist import LinkedList
+
+
+class LinkedQueue:
+    def __init__(self):
+        self._L = LinkedList()
+
+    def enque(self, item):
+        self._L.add_last(item)
+
+    def dequeue(self):
+        return self._L.remove_first()
+
+    def peek(self):
+        # or we have to use remove_first() and add_first() to get item at head
+        return self._L.get_head()
+
+    def __len__(self):
+        return len(self._L)
+
+    def is_empty(self):
+        return len(self) == 0
