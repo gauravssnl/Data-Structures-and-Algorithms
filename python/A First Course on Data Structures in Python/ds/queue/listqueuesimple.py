44b5269711a7f26a7003532c624f7ec9f7020c34
Gaurav <gaurav.ssnl@gmail.com>
2020-12-22 00:16:27 +0530

    A simple Queue using list example added.

---
+++ python/A First Course on Data Structures in Python/ds/queue/listqueuesimple.py
@@ -0,0 +1,18 @@
+class ListQueueSimple:
+    def __init__(self):
+        self._L = []
+
+    def enqueue(self, item):
+        self._L.append(item)
+
+    def dequeue(self):
+        return self._L.pop(0)
+
+    def peek(self):
+        return self._L[0]
+
+    def __len__(self):
+        return len(self._L)
+
+    def isempty(self):
+        return len(self) == 0
