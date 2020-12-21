f1fa6986fa95c6e923cfe43754d72bb6989e0f1a
Gaurav <gaurav.ssnl@gmail.com>
2020-12-22 01:30:53 +0530

    Add simple ListDeque example.

---
+++ python/A First Course on Data Structures in Python/ds/deque/simplelistdeque.py
@@ -0,0 +1,15 @@
+class SimpleListDeque:
+    def __init__(self):
+        self._L = []
+
+    def add_first(self, item):
+        self._L.insert(0, item)
+
+    def add_last(self, item):
+        self._L.append(item)
+
+    def remove_first(self):
+        return self._L.pop(0)
+
+    def remove_last(self):
+        return self._L.pop()
